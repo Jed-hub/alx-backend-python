45400ff7704f1c0598a2cd69430cd111fd00b296
root <jedidiabahena@gmail.com>
2023-01-09 18:25:21 +0000

    added answer 1

---
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+""" Concurrent coroutiines module """
+import asyncio
+import random
+from typing import List
+
+
+wait_random = __import__('0-basic_async_syntax').wait_random
+
+async def wait_n(n: int = 0, max_delay: int = 10) -> List[float]:
+    """
+    Returns the list of all delays
+    """
+    delays = []
+    tasks = []
+
+    for _ in range(n):
+        tasks.append(wait_random(max_delay))
+
+    for task in asyncio.as_completed((tasks)):
+        delay = await task
+        delays.append(delay)
+
+    return delays
